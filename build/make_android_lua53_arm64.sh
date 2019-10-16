6171274e0a3d874d461d0cc37115d5627e62aa2b
Xpol Wan <xpolife@gmail.com>
2019-10-16 19:17:30 +0800

    Refactor build bash file for Android with  arm64.

--- build/make_android_lua53_arm64.sh
+++ build/make_android_lua53_arm64.sh
@@ -1,26 +1,22 @@
-if [ -z "$ANDROID_NDK" ]; then
-    export ANDROID_NDK=~/android-ndk-r15c
+if [ -n "$ANDROID_NDK" ]; then
+    export NDK=${ANDROID_NDK}
+elif [ -n "$ANDROID_NDK_HOME" ]; then
+    export NDK=${ANDROID_NDK_HOME}
+else
+    export NDK=~/android-ndk-r16b
 fi
 
-mkdir -p build_v7a && cd build_v7a
-cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang -DANDROID_NATIVE_API_LEVEL=android-9 ../
-cd ..
-cmake --build build_v7a --config Release
-mkdir -p plugin_lua53/Plugins/Android/libs/armeabi-v7a/
-cp build_v7a/libxlua.so plugin_lua53/Plugins/Android/libs/armeabi-v7a/libxlua.so
+function build() {
+    API=$1
+    ABI=$2
+    TOOLCHAIN_ANME=$3
+    BUILD_PATH=build.${ABI}
+    cmake -H. -B${BUILD_PATH} -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${API} -DANDROID_TOOLCHAIN_NAME=${TOOLCHAIN_ANME}
+    cmake --build ${BUILD_PATH} --config Release
+    mkdir -p plugin_lua53/Plugins/Android/libs/${ABI}/
+    cp ${BUILD_PATH}/libxlua.so plugin_lua53/Plugins/Android/libs/${ABI}/libxlua.so
+}
 
-mkdir -p build_v8a && cd build_v8a
-cmake -DANDROID_ABI=arm64-v8a -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang -DANDROID_NATIVE_API_LEVEL=android-9 ../
-cd ..
-cmake --build build_v8a --config Release
-mkdir -p plugin_lua53/Plugins/Android/libs/arm64-v8a/
-cp build_v8a/libxlua.so plugin_lua53/Plugins/Android/libs/arm64-v8a/libxlua.so
-
-mkdir -p build_x86 && cd build_x86
-cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-clang -DANDROID_NATIVE_API_LEVEL=android-9 ../
-cd ..
-cmake --build build_x86 --config Release
-mkdir -p plugin_lua53/Plugins/Android/libs/x86/
-cp build_x86/libxlua.so plugin_lua53/Plugins/Android/libs/x86/libxlua.so
-
-
+build android-16 arm64-v8a aarch64-linux-android-4.9
+build android-16 armeabi-v7a arm-linux-androideabi-4.9
+build android-16 x86 x86-4.9
